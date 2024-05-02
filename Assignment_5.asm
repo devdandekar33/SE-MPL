73b0af005850227f3c1e9c4de1f069f6c28a539d
devdandekar33 <138193237+devdandekar33@users.noreply.github.com>
2024-05-02 13:40:04 +0530

    Update Assignment_5.asm

--- Assignment_5.asm
+++ Assignment_5.asm
@@ -1,97 +1,78 @@
-Assignment 5
 section .data
-    msg:db"Storing the data in array",0xA,0xD
-    msglen equ $-msg
-    msg1: db"Printing : ",0xA, 0xD  ;to print array
-    msg1len equ $-msg1
-    msg2:db"Positive numbers are ",0xA, 0xD  ;to print positive
-    msg2len equ $-msg2
-    msg3:db"Negative numbers are ",0xA,0xD ; to print negative
-    msg3len equ $-msg3
-    array db 00044230h,00000099h,00000080h,00000001h,00000021h    
+msg :db"Positive Numbers", 10,13
+msglen equ $-msg
+
+msg1 :db"Negative Numbers", 10,13
+msg1len equ $-msg1
+
+msg2 :db"Numbers in array"
+msg2len equ $-msg2
+
+
+nmsg :db"", 10,13
+nmsglen equ $-nmsg
+
+;n equ 5
+
+array db -0000000000000000h,-0000000000000010h,000000000000010h,-000000000000001h,-0000000000000000h
+
 section .bss
-    %macro exit 0
-    mov rax,60
-    mov rdi,0
-    syscall    
-    %endmacro    
-    %macro print 2
-    mov rax,1
-    mov rdi,1
-    mov rsi,%1
-    mov rdx,%2
-    syscall
-    %endmacro
-    result resb 20
-    counter resb 1
-    ncount resb 1
-    pcount resb 1
+n resb 1
+%macro display 2
+mov rax,1
+mov rdi,1
+mov rsi,%1
+mov rdx,%2
+syscall
+%endmacro
+
+%macro exit 0
+mov rax,60
+mov rdi,0
+syscall
+%endmacro
+
+result resb 16
+pcount resb 1
+ncount resb 1
+
+
 section .text
-global_start:
-    _start:
-    
-   	 print msg,msglen  ;print elements in the array
-   	 mov byte[counter],5   ;set counter to 5
-   	 mov rbp,array      	;declaring array
-
-    arrayprint:
-   	 mov al,[rbp] ;al is lower of ax points to array
-   	 call display ;display is a subroutine
-   	 inc rbp ; increment to next element of array
-   	 dec byte[counter] ; decrement the counter
-   	 jnz arrayprint ; if not zero jump to arrayprint
-   	 
-   	 mov byte[counter],5   ;again set counter to 5
-   	 mov rbp,array      	;declaring array
-   	 
-   	 mov byte[ncount],00h    ;set ncount to 5
-   	 mov byte[pcount],00h    ;set pcount to 5
-    
-    logic:
-   	 mov al, byte[rbp] ; put the pointer of rbp in al
-   	 rcl al, 1    ; rotate to left with carry by 1
-   	 jc go    	 ; jump to go if carry
-         inc byte[pcount] ; if not carry inc pcount
-   	 jmp skip  	 ; if pcount is incremented then dont increment ncount
-    go:    inc byte[ncount]    ;inc ncount
-    skip:    inc rbp   	 ;now set rbp to next digit
-   	 dec byte[counter]    ;dec counter
-   	 jnz logic    ;if counter is not zero jump to logic
-   	 
-   	 print msg2, msglen2 ; print the message
-   	 mov al, byte[pcount] ;move the pcount to al
-   	 call display ;call display subroutine
-   	 print msg1, msglen1 ; print
-   	 
-   	 
-   	 print msg3, msglen3 ; print the message of negative count
-   	 mov al, byte[ncount]
-   	 call display
-   	 print msg1, msglen1
-   	 
-   	 exit
-   	 
-display: 	 mov rbx,rax    ;
-   	 mov rdi,result ;result will be stored in rdi
-   	 mov cx,16 ;set cx equal to 16
-    convert:rol rbx,04 ;rotate rbx by 4
-   	 mov al,bl
-   	 and al,0fh ; and al with 00001111
-   	 cmp al, 09h ; compare the al with 09
-   	 jle add_30 ;jump to add_30 if al <=9
-   	 add al,07h ;else add 07 to al
-    add_30: add al, 30h ; all 30 in all cases
-   	 mov [rdi], al ;move al to rdi pointer
-   	 inc rdi ; increment rdi to get next result
-   	 dec cx ;dec cx couter
-   	 jnz convert ; jump if not zero to convert
-   	 
-   	 print result,16
-   	 ret
-   	 
-   	 
-
-    
-
+global _start
+ 	_start:
+ 	display msg2,msg2len
+ 	mov byte[n],05h			;counter for array
+ 	mov rbp,array				;rbp points to array
+ b1:	mov al,[rbp]				;mov array pointer to al
+ 	call Display				;call display procedure
+ 	inc rbp					;increment pointer
+ 	;loop b1
+ 	dec byte[n]
+ 	jnz b1
+ 	
+ 	mov byte[n],05
+ 	mov rsi,array				;set array pointer to rsi(source index)
+	mov byte[pcount],00h			;set pcount to 0
+ 	mov byte[ncount],00h			;set ncount to 0
+ 	
+ back: mov al,byte[rsi]				;mov first element of array to al
+       cmp al,0					;compare al with 0
+       jg positive
+       inc byte[ncount]				; if al<0 ncount
+       jmp next
+       
+       
+positive:inc byte[pcount]
+   next:inc rsi					;increment rsi
+	dec byte[n]
+	jnz back
 	
-
+	display msg,msglen		
+	mov al,byte[pcount]		;move value in pcount to al
+	call Display
+	
+	display msg1,msg1len
+	mov al,byte[ncount]
+	call Display
+ 	
+ 	exit
