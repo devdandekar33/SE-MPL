ea1f7dd0d0a64a9905cf1e6402321da5ac9d144c
devdandekar33 <138193237+devdandekar33@users.noreply.github.com>
2024-05-02 17:06:11 +0530

    Update Assignment_4.asm

--- Assignment_4.asm
+++ Assignment_4.asm
@@ -1,129 +1,191 @@
+%macro scall 4
+        mov rax,%1
+        mov rdi,%2
+        mov rsi,%3
+        mov rdx,%4
+        syscall
+%endmacro
+
 section .data
-msg :db 10,13,"Menu",10,13,"1.Add",10,13,"2.sub",10,13,"3.Mul",10,13,"4.Div",10,13,"5. exit",10,13,"enter choice->"
-msglen equ $-msg
+        arr dq 000000000000005h,0000000000000002h
+        n equ 2
 
-msg1: db "First Number",10,13
-m1len equ $-msg1
+        menu db 10d,13d,"**********MENU**********"
+             db 10d,13d,"1. Addition"
+             db 10d,13d,"2. Subtraction"
+             db 10d,13d,"3. Multiplication"
+             db 10d,13d,"4. Division"
+             db 10d,13d,"5. Exit"
+             db 10d,13d,"Enter your Choice: "
+        menu_len equ $-menu
 
-msg2: dw "Second number" ,10d,13d
-m2len equ $-msg2
-msg3: db "Result" ,10d,13d
-m3len equ $-msg3
-
-
-nmsg :db"", 10,13
-nmsglen equ $-nmsg
-
-array db 0000000000000004h,0000000000000003h
+        m1 db 10d,13d,"Addition: "
+        l1 equ $-m1
+        m2 db 10d,13d,"Substraction: "
+        l2 equ $-m2
+        m3 db 10d,13d,"Multiplication: "
+        l3 equ $-m3
+        m4 db 10d,13d,"Division: "
+        l4 equ $-m4
+m5 db 10d,13d,"The numbers are 000000000000003h,0000000000000002h"
+l5 equ $-m5
 
 section .bss
-n resb 2
-menu resb 1
-result resb 16
-
-%macro scall 3
-mov rax,%1
-mov rdi,%1
-mov rsi,%2
-mov rdx,%3
-syscall
-%endmacro
-
-%macro exit 0
-mov rax,60
-mov rdi,0
-syscall
-%endmacro
+        answer resb 16 ;to store the result of operation
+        choice resb 2
 
 section .text
-global _start
- 	_start:
- up:	  scall 1,msg,msglen
- 	  scall 0,menu,1
- 	  
- 	  
- 	  cmp byte[menu],1
- 	  jz case1
- 	  
- 	  cmp byte[menu],2
- 	  jz case2
- 	  
- 	  
- 	  
- 	  
- 	  cmp byte[menu],'5'
- 	  je case5
- 	  
- 	  
- 	  
- 	  
- 	  
- 	
-case1: scall 1,msg1,m1len
-       mov rbp, array
-          
-       mov al,byte[rbp]
-       call Display
-       
-       inc rbp
-       
-      scall 1,msg2,m2len
-      mov al,byte[rbp]
-       call Display
-       
-       call Add
-       jmp up
- 	
- 	
-case5:  exit
- 	
-case2: scall 1,msg1,msglen
-	mov rbp,array
-	mov al,byte[rbp]
-	call Display
-	
-	inc rbp
-	scall 1,msg2,m2len
-	mov al,byte[rbp]
-	call Display
-	
-	call Sub1
-	jmp up
-	
-Sub1:  scall 1,msg3,m3len
-	mov rdx,array
-	mov al,byte[rdx]
-	inc rdx
-	mov bl,byte[rdx]
-	sub al,bl
-	call Display
-	   ret
- 
- Add: scall 1,msg3,m3len
-      mov rdx,array
-      mov al,byte[rdx]
-      inc rdx
-      mov bl,byte[rdx]
-      add al,bl
-      call Display
-           ret 
- 	
- 	
-Display: 	mov rbx, rax
-		mov rdi, result
-		mov cx, 16
-	convert:rol rbx, 04
-		mov al, bl
-		and al, 0fh
-		cmp al, 09h
-		jle add_30
-		add al, 07h
-	add_30: add al, 30h
-		mov [rdi], al
-		inc rdi
-		dec cx
-		jnz convert
-		
-		scall 1,result, 16
-		scall 1,nmsg, nmsglen
-		ret
+        global _start:
+        _start:
+
+        up: scall 1,1,menu,menu_len
+       scall 0,0,choice,2
+
+        cmp byte[choice],'1'
+        je case1
+        cmp byte[choice],'2'
+        je case2
+        cmp byte[choice],'3'
+        je case3
+        cmp byte[choice],'4'
+        je case4
+        cmp byte[choice],'5'
+        je case5
+
+
+
+        case1: scall 1,1,m5,l5
+        scall 1,1,m1,l1
+                call addition
+                jmp up
+
+        case2: scall 1,1,m5,l5
+        scall 1,1,m2,l2
+ call substraction
+ jmp up
+
+        case3: scall 1,1,m5,l5
+        scall 1,1,m3,l3
+ call multiplication
+ jmp up
+
+        case4: scall 1,1,m5,l5
+        scall 1,1,m4,l4
+ call division
+ jmp up
+
+        case5:  mov rax,60
+        mov rdi,0
+                syscall
+
+
+
+;procedures for arithmetic and logical operations
+addition:
+        mov rcx,n				;?
+        dec rcx					;?
+        mov rsi,arr
+        mov rax,[rsi]    			; mov first number in rax                       
+up1:    add rsi,8
+        mov rbx,[rsi]				; mov second number in rbx
+        add rax,rbx
+        loop up1
+        call display
+ret
+
+
+substraction:
+		; note that rcx is mostly used as loop counter
+        mov rcx,n			;n is size of array i.e.2 
+        dec rcx				;Decrements the value in rcx by 1. Since rcx holds the array size, this effectively prepares for a loop that iterates n-1 times (excluding the last element)
+        mov rsi,arr			;set rsi pointer to beginning of array
+        mov rax,[rsi]
+up2:    add rsi,8
+        mov rbx,[rsi]
+        sub rax,rbx
+        loop up2
+        call display
+ret
+
+
+multiplication:
+        mov rcx,n
+        dec rcx
+        mov rsi,arr
+        mov rax,[rsi]
+up3:    add rsi,8
+        mov rbx,[rsi]
+        mul rbx
+        loop up3
+        call display
+ret
+
+division:
+        mov rcx,n
+        dec rcx
+
+        mov rsi,arr
+        mov rax,[rsi]
+up4:    add rsi,8
+        mov rbx,[rsi]
+        mov rdx,0
+        div rbx
+        loop up4
+        call display
+ret
+
+or:
+        mov rcx,n
+        dec rcx
+        mov rsi,arr
+        mov rax,[rsi]
+up6:    add rsi,8
+        mov rbx,[rsi]
+        or rax,rbx
+        loop up6
+        call display
+ret
+
+xor:
+        mov rcx,n
+        dec rcx
+        mov rsi,arr
+        mov rax,[rsi]
+up7:    add rsi,8
+        mov rbx,[rsi]
+        xor rax,rbx
+        loop up7
+        call display
+ret
+
+and:
+        mov rcx,n
+        dec rcx
+        mov rsi,arr
+        mov rax,[rsi]
+up8:    add rsi,8
+        mov rbx,[rsi]
+        and rax,rbx
+        loop up8
+        call display
+ret
+
+
+display:
+        mov rsi,answer+15
+        mov rcx,16
+;converting answer in rax to hex
+cnt:    mov rdx,0			;remainder is stored in rdx
+        mov rbx,16
+        div rbx
+;converting hex to ascii hex
+        cmp dl,09h
+        jbe add30
+        add dl,07h
+add30:  add dl,30h
+        mov [rsi],dl
+        dec rsi
+        dec rcx
+        jnz cnt
+        scall 1,1,answer,16
+ret
